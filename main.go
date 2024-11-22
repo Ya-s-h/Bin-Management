cd63e421ef7faa735c20c1054ad3fd6b77f9e42a
Yash Aggarwal <vasu2013agg@gmail.com>
2024-11-23 03:40:16 +0530

    middleware added

--- main.go
+++ main.go
@@ -6,10 +6,20 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(healthcheck.New(healthcheck.Config{
+		LivenessProbe: func(c *fiber.Ctx) bool {
+			return true
+		},
+		LivenessEndpoint: "/live",
+	}))
+
 	pg.ConnectToDb()
 	route.UserSetupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
